71313feddca9010f7343fc37885ab2bc69b6ea16
tomekmazur95 <tomek.mazur95@gmail.com>
2024-07-05 23:28:26 +0200

    add measurement tests

--- src/test/java/com/crud/api/service/integration/controller/MeasurementControllerTestIT.java
+++ src/test/java/com/crud/api/service/integration/controller/MeasurementControllerTestIT.java
@@ -2,10 +2,18 @@ package com.crud.api.service.integration.controller;
 
 
 import com.crud.api.dto.RequestMeasurementDTO;
+import com.crud.api.entity.User;
+import com.crud.api.entity.UserInfo;
+import com.crud.api.enums.Activity;
+import com.crud.api.enums.Gender;
+import com.crud.api.enums.MeasureType;
+import com.crud.api.enums.Unit;
+import com.crud.api.repository.MeasurementRepository;
 import com.crud.api.repository.UserInfoRepository;
 import com.crud.api.repository.UserRepository;
 import com.crud.api.service.integration.AppMySQLContainer;
 import com.crud.api.service.integration.DatabaseSetupExtension;
+import com.crud.api.service.integration.helper.TestEntityFactory;
 import com.crud.api.service.integration.helper.TestJsonMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
@@ -42,8 +50,12 @@ public class MeasurementControllerTestIT extends AppMySQLContainer {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private MeasurementRepository measurementRepository;
+
     @AfterEach
     public void clean() {
+        measurementRepository.deleteAll();
         userRepository.deleteAll();
         userInfoRepository.deleteAll();
     }
@@ -81,5 +93,27 @@ public class MeasurementControllerTestIT extends AppMySQLContainer {
         Assertions.assertEquals(String.format("User with id: %s not found", userId), errorMessage);
         Assertions.assertTrue(userRepository.findById(userId).isEmpty());
         Assertions.assertEquals(404, status);
+    }
+
+    @Test
+    @WithMockUser(authorities = {"USER"})
+    void shouldCreateMeasurement() throws Exception {
+        UserInfo userInfoDomain = TestEntityFactory.createUserInfoDomain("john@gmail.com", "password123");
+        userInfoRepository.save(userInfoDomain);
+        User userDomain = TestEntityFactory.createUserDomain("John", Gender.MALE, Activity.EXTRA_ACTIVE, 53);
+        userDomain.setUserInfo(userInfoDomain);
+        userRepository.save(userDomain);
+
+        long userId = userDomain.getId();
+        RequestMeasurementDTO requestMeasurementDTO = TestEntityFactory.createRequestMeasurementDTO(MeasureType.HEIGHT, Unit.CENTIMETERS, 183.0);
+
+        mockMvc.perform(post("/user/{userId}/measurements", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestJsonMapper.asJsonString(requestMeasurementDTO)))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(status().isCreated());
+
+        Assertions.assertTrue(measurementRepository.existsByUserId(userId));
     }
 }
