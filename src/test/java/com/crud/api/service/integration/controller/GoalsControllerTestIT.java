de6866aad873b376be11a210d68fed429d61a96a
tomekmazur95 <tomek.mazur95@gmail.com>
2024-07-18 23:12:37 +0200

    add test in GoalsController

--- src/test/java/com/crud/api/service/integration/controller/GoalsControllerTestIT.java
+++ src/test/java/com/crud/api/service/integration/controller/GoalsControllerTestIT.java
@@ -1,9 +1,17 @@
 package com.crud.api.service.integration.controller;
 
 
+import com.crud.api.entity.User;
+import com.crud.api.entity.UserInfo;
+import com.crud.api.enums.Activity;
+import com.crud.api.enums.Gender;
+import com.crud.api.enums.MeasureType;
+import com.crud.api.repository.MeasurementRepository;
+import com.crud.api.repository.UserInfoRepository;
 import com.crud.api.repository.UserRepository;
 import com.crud.api.service.integration.AppMySQLContainer;
 import com.crud.api.service.integration.DatabaseSetupExtension;
+import com.crud.api.service.integration.helper.TestEntityFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,6 +26,8 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.Objects;
 
+import static com.crud.api.service.integration.helper.TestEntityFactory.createUserDomain;
+import static com.crud.api.service.integration.helper.TestEntityFactory.createUserInfoDomain;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,9 +44,17 @@ public class GoalsControllerTestIT extends AppMySQLContainer {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserInfoRepository userInfoRepository;
+
+    @Autowired
+    MeasurementRepository measurementRepository;
+
     @AfterEach
     public void clean() {
+        measurementRepository.deleteAll();
         userRepository.deleteAll();
+        userInfoRepository.deleteAll();
     }
 
     @Test
@@ -66,6 +84,29 @@ public class GoalsControllerTestIT extends AppMySQLContainer {
         String errorMessage = Objects.requireNonNull(result.getResolvedException()).getMessage();
         Assertions.assertEquals(String.format("User with id: %s not found", userId), errorMessage);
         Assertions.assertTrue(userRepository.findById(userId).isEmpty());
+        Assertions.assertEquals(404, status);
+    }
+
+    @Test
+    @WithMockUser(authorities = {"USER"})
+    void shouldThrowMeasurementExceptionWhenCurrentWeightNotFound() throws Exception {
+        UserInfo userInfoDomain = createUserInfoDomain("john@gmail.com", "password123");
+        userInfoRepository.save(userInfoDomain);
+        User userDomain = createUserDomain("John", Gender.MALE, Activity.EXTRA_ACTIVE, 53);
+        userDomain.setUserInfo(userInfoDomain);
+        userRepository.save(userDomain);
+
+        long userId = userDomain.getId();
+
+        MvcResult result = mockMvc.perform(get("/goals/{userId}", userId))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        String errorMessage = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        Assertions.assertEquals(String.format("Measurement Type: %s not found for User with id %s", MeasureType.CURRENT_WEIGHT, userId), errorMessage);
         Assertions.assertEquals(404, status);
     }
 
