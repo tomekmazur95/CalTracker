2c73b8f3350ad363719a59f888c6cf99e88dc053
tomekmazur95 <tomek.mazur95@gmail.com>
2024-07-18 23:01:10 +0200

    add test in GoalsController

--- src/test/java/com/crud/api/service/integration/controller/GoalsControllerTestIT.java
+++ src/test/java/com/crud/api/service/integration/controller/GoalsControllerTestIT.java
@@ -1,17 +1,22 @@
 package com.crud.api.service.integration.controller;
 
 
+import com.crud.api.repository.UserRepository;
 import com.crud.api.service.integration.AppMySQLContainer;
 import com.crud.api.service.integration.DatabaseSetupExtension;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Objects;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,6 +31,14 @@ public class GoalsControllerTestIT extends AppMySQLContainer {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    public void clean() {
+        userRepository.deleteAll();
+    }
+
     @Test
     void shouldThrowForbiddenExceptionWhenUserIsUnauthorized() throws Exception {
         long userId = 10;
@@ -37,6 +50,23 @@ public class GoalsControllerTestIT extends AppMySQLContainer {
 
         int status = result.getResponse().getStatus();
         Assertions.assertEquals(403, status);
+    }
+
+    @Test
+    @WithMockUser(authorities = {"USER"})
+    void shouldThrowUserNotFoundExceptionInFoundUserGoalsMethod() throws Exception {
+        long userId = 1;
+        MvcResult result = mockMvc.perform(get("/goals/{userId}", userId))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        int status = result.getResponse().getStatus();
+        String errorMessage = Objects.requireNonNull(result.getResolvedException()).getMessage();
+        Assertions.assertEquals(String.format("User with id: %s not found", userId), errorMessage);
+        Assertions.assertTrue(userRepository.findById(userId).isEmpty());
+        Assertions.assertEquals(404, status);
     }
 
 }
