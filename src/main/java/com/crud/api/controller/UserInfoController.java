95f8b835ead5c811bd4270fe789a54759c1fb295
tomekmazur95 <tomek.mazur95@gmail.com>
2024-07-22 23:23:01 +0200

    add api documentation for UserInfoController

--- src/main/java/com/crud/api/controller/UserInfoController.java
+++ src/main/java/com/crud/api/controller/UserInfoController.java
@@ -1,5 +1,6 @@
 package com.crud.api.controller;
 
+import com.crud.api.controller.swagger.UserInfoControllerSwagger;
 import com.crud.api.dto.UserInfoResponse;
 import com.crud.api.service.UserInfoService;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/userInfo")
 @RequiredArgsConstructor
-public class UserInfoController {
+public class UserInfoController implements UserInfoControllerSwagger {
 
     private final UserInfoService userInfoService;
 
+    @Override
     @GetMapping()
     public ResponseEntity<UserInfoResponse> findByUser() {
         return ResponseEntity.ok(userInfoService.findUser());
