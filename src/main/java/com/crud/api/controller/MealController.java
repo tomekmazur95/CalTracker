ed9361e732a4165cb3267c37a1f2c6a4fe1f730a
tomekmazur95 <tomek.mazur95@gmail.com>
2024-07-28 22:56:30 +0200

    add api documentation for MealController

--- src/main/java/com/crud/api/controller/MealController.java
+++ src/main/java/com/crud/api/controller/MealController.java
@@ -1,5 +1,6 @@
 package com.crud.api.controller;
 
+import com.crud.api.controller.swagger.MealControllerSwagger;
 import com.crud.api.dto.RequestMealDTO;
 import com.crud.api.dto.ResponseMealDTO;
 import com.crud.api.service.MealService;
@@ -14,16 +15,18 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/meals")
-public class MealController {
+public class MealController implements MealControllerSwagger {
 
     private final MealService mealService;
 
+    @Override
     @PostMapping()
     @PreAuthorize(value = "hasAnyAuthority('ADMIN', 'USER')")
     public ResponseEntity<ResponseMealDTO> createMeal(@RequestBody RequestMealDTO foodList, @RequestParam Long userId) {
         return new ResponseEntity<>(mealService.createMeal(foodList, userId), HttpStatus.CREATED);
     }
 
+    @Override
     @GetMapping()
     @PreAuthorize(value = "hasAnyAuthority('ADMIN', 'USER')")
     public ResponseEntity<List<ResponseMealDTO>> findAllUserMeals(@RequestParam Long userId) {
--- src/main/java/com/crud/api/controller/swagger/MealControllerSwagger.java
+++ src/main/java/com/crud/api/controller/swagger/MealControllerSwagger.java
@@ -1,7 +1,7 @@
 package com.crud.api.controller.swagger;
 
-import com.crud.api.dto.RequestFoodDTO;
-import com.crud.api.dto.ResponseFoodDTO;
+import com.crud.api.dto.RequestMealDTO;
+import com.crud.api.dto.ResponseMealDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -10,31 +10,32 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-public interface FoodControllerSwagger {
-
+public interface MealControllerSwagger {
     @Operation(
-            summary = "Create Food",
-            description = "Create food in the system for specified User"
+            summary = "Create Meal",
+            description = "Create meal in the system for specified User"
     )
     @SecurityRequirement(name = "Bearer Authentication")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Operation successful, food created"),
+            @ApiResponse(responseCode = "201", description = "Operation successful, meal created"),
             @ApiResponse(responseCode = "400", description = "Bad request, invalid input"),
             @ApiResponse(responseCode = "401", description = "In case of unauthorized access"),
+            @ApiResponse(responseCode = "404", description = "In case of User or Food not found"),
             @ApiResponse(responseCode = "500", description = "In case of any exception")
     })
-    ResponseEntity<ResponseFoodDTO> createFood(RequestFoodDTO dto, Long userId);
+    ResponseEntity<ResponseMealDTO> createMeal(RequestMealDTO foodList, Long userId);
 
     @Operation(
-            summary = "Retrieve All foods for a User",
-            description = "Fetches all foods associated with the specified User ID"
+            summary = "Retrieve All meals for a User",
+            description = "Fetches all meals associated with the specified User ID"
     )
     @SecurityRequirement(name = "Bearer Authentication")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Operation successful, returns list of foods"),
+            @ApiResponse(responseCode = "200", description = "Operation successful, returns list of meals"),
+            @ApiResponse(responseCode = "400", description = "Bad request, invalid input"),
             @ApiResponse(responseCode = "401", description = "In case of unauthorized access"),
-            @ApiResponse(responseCode = "404", description = "User not found for the given ID"),
+            @ApiResponse(responseCode = "404", description = "In case of User or Food not found"),
             @ApiResponse(responseCode = "500", description = "In case of any exception")
     })
-    ResponseEntity<List<ResponseFoodDTO>> findUserFoods(Long userId);
+    ResponseEntity<List<ResponseMealDTO>> findAllUserMeals(Long userId);
 }
